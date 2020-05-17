94cfeadf09b8f47faf932924cf4af4dca8033801
W11ld33r <w11ld33r@gmail.com>
2020-05-16 20:11:07 -0500

    agregado estilos:bootstrap

--- ch1-movie-list/src/index.js
+++ ch1-movie-list/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './containers/List';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const App = () => {
 	return <List />;
