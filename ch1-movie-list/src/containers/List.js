541cbbf0f1b211ba17ee37e5bb969f90ac67b56f
W11ld33r <w11ld33r@gmail.com>
2020-05-16 20:03:39 -0500

    agregado components card & list

---
+++ ch1-movie-list/src/containers/List.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import Card from '../components/Card/Card';
+
+class List extends Component {
+	
+	constructor() {
+		super();
+		this.state = {
+			movies: [],
+			loading: true,
+		};
+	}
+
+	async componentDidMount() {
+		const movies = await fetch('../../assets/data.json');
+		const moviesJSON = await movies.json();
+
+		if (!moviesJSON) return;
+
+		this.setState({
+			movies: moviesJSON,
+			loading: false,
+		});
+	}
+
+	render() {
+		const { movies, loading } = this.state;
+
+		if (loading) {
+			return <div>Loading...</div>;	
+		}
+
+		return movies.map(movie => <Card key={ movie.id } movie={ movie } />);
+	}
+}
+
+export default List;
