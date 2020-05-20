2cd82d1f3c630602f8772b4eb0df214c6bc899de
W11ld33r <w11ld33r@gmail.com>
2020-05-19 20:59:37 -0500

    agregado HOC para traer datos

--- project-management-board/src/containers/Board.js
+++ project-management-board/src/containers/Board.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+
+import withDataFetching from '../withDataFetching';
+
 import Lane from '../components/Lane/Lane';
 
 const BoardWrapper = styled.div`
@@ -13,64 +16,22 @@ const BoardWrapper = styled.div`
   }
 `;
 
-class Board extends Component {
+const Board = ({ lanes, loading, error, data }) => (
 
-  constructor() {
-    super();
-
-    this.state = {
-      data: [],
-      loading: true,
-      error: ''
-    };
-  }
-
-  async componentDidMount() {
-    try {
-
-      const tickets = await fetch('../../assets/data.json');
-      const ticketsJSON = await tickets.json();
-
-      if (ticketsJSON) {
-        this.setState({
-          data: ticketsJSON,
-          loading: false
-        });
-      }
-
-    } catch(error) {
-      this.setState({
-        loading: false,
-        error: error.message
-      });
+  <BoardWrapper>
+    {
+      lanes.map(lane => 
+        <Lane 
+          key={lane.id}
+          title={lane.title}
+          loading={loading}
+          error={error}
+          tickets={data.filter(ticket => ticket.lane === lane.id)}
+        />
+      )
     }
-  } 
+  </BoardWrapper>
 
-  render() {
+);
 
-    const { data, loading, error } = this.state;
-
-    const lanes = [
-      { id: 1, title: 'To Do' },
-      { id: 2, title: 'In Progress' },
-      { id: 3, title: 'Review' },
-      { id: 4, title: 'Done' },
-    ];
-
-    return (
-      <BoardWrapper>
-        {lanes.map(lane => (
-          <Lane 
-            key={lane.id} 
-            title={lane.title}
-            loading={loading}
-            error={error}
-            tickets={ data.filter(ticket => ticket.lane === lane.id) }
-          />
-        ))}
-      </BoardWrapper>
-    );
-  }
-}
-
-export default Board;
+export default withDataFetching(Board);
